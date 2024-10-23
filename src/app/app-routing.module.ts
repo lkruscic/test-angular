7135c75f228aad2a1c96c9e10f1215638dc2fdf8
INFINEON\kruscicl <kruscic.external@infineon.com>
2024-10-23 09:53:01 +0200

    testing monaco editor with ngx-monaco-editor-v2 wrapper

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MonacoDiffEditorComponent} from "./monaco-diff-editor/monaco-diff-editor.component";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'monaco-diff-editor', component: MonacoDiffEditorComponent },
+  { path: 'app', component:  AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,65 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonacoDiffEditorComponent } from './monaco-diff-editor/monaco-diff-editor.component';
+import {MonacoEditorModule, NgxMonacoEditorConfig} from "ngx-monaco-editor-v2";
+import {FormsModule} from "@angular/forms";
+
+declare var monaco: any;
+
+export function onMonacoLoad() {
+
+  console.log((window as any).monaco);
+
+  const uri = monaco.Uri.parse('a://b/foo.json');
+  monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+    validate: true,
+    schemas: [{
+      uri: 'http://myserver/foo-schema.json',
+      fileMatch: [uri.toString()],
+      schema: {
+        type: 'object',
+        properties: {
+          p1: {
+            enum: ['v1', 'v2']
+          },
+          p2: {
+            $ref: 'http://myserver/bar-schema.json'
+          }
+        }
+      }
+    }, {
+      uri: 'http://myserver/bar-schema.json',
+      fileMatch: [uri.toString()],
+      schema: {
+        type: 'object',
+        properties: {
+          q1: {
+            enum: ['x1', 'x2']
+          }
+        }
+      }
+    }]
+  });
+
+}
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  baseUrl: 'assets',
+  defaultOptions: { scrollBeyondLastLine: false },
+  onMonacoLoad
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonacoDiffEditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MonacoEditorModule.forRoot(monacoConfig),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
